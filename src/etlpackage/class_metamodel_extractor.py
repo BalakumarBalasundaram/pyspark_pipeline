d8cb30bda530f30b95614557ba896e5663fc9fc4
Balakumar Balasundaram <balakumar.b05@gmail.com>
2022-03-24 23:01:56 +0100

    optimized metamodel extractor code

--- src/etlpackage/class_metamodel_extractor.py
+++ src/etlpackage/class_metamodel_extractor.py
@@ -2,26 +2,19 @@ from pyspark import SparkContext
 from pyspark.sql import DataFrame, SparkSession
 
 
-def get_metamodel_by_type(metamodel_df: DataFrame, entity_type: str):
-    return "dataframe"
-
-
 class MetamodelExtractor:
     def __init__(self, spark: SparkSession, schema, lookup_filepath):
         self.spark = spark
         self.metamodel_schema = schema
         self.metamodel_lookup = lookup_filepath
+        self.metamodel_full_df = (spark.read  # The DataFrameReader
+                                  .option("sep", "|")  # Use tab delimiter (default is comma-separator)
+                                  .schema(self.metamodel_schema)  # Apply schema
+                                  .csv(self.metamodel_lookup))
 
     def extract_metamodel_df_by_stream_id(self, src_stream_id):
         try:
-            metamodel_full_df = (self.spark.read  # The DataFrameReader
-                                 .option("sep", "|")  # Use tab delimiter (default is comma-separator)
-                                 # .option("header", "true")   # Use first line of all files as header
-                                 .schema(self.metamodel_schema)  # Apply schema
-                                 .csv(self.metamodel_lookup)  # Creates a DataFrame from CSV after reading in the file
-                                 )
-            # metamodel_full_df.show(vertical=True)
-            metamodel_df = metamodel_full_df.filter(metamodel_full_df.SRC_STREAM_ID == src_stream_id)
+            metamodel_df = self.metamodel_full_df.filter(self.metamodel_full_df.SRC_STREAM_ID == src_stream_id)
             metamodel_df.show(vertical=True)
             return metamodel_df
         except:
